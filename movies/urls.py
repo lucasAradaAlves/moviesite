1ad4e064713ec4722ca54646814b2987239122dd
Andre Sato <andre.kubagawa@usp.br>
2021-10-11 17:41:46 -0300

    Adicionar pasta de listagem de filmes usando passagem de contexto

--- movies/urls.py
+++ movies/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 app_name = 'movies'
 urlpatterns = [
+    path('', views.list_movies, name='index'),
     path('<int:movie_id>/', views.detail_movie, name='detail'),
 ]
--- movies/views.py
+++ movies/views.py
@@ -1,8 +1,14 @@
 from django.http import HttpResponse
 from .temp_data import movie_data
+from django.shortcuts import render
 
 
 def detail_movie(request, movie_id):
     movie = movie_data[movie_id - 1]
     return HttpResponse(
         f'Detalhes do filme {movie["name"]} ({movie["release_year"]})')
+
+
+def list_movies(request):
+    context = {"movie_list": movie_data}
+    return render(request, 'movies/index.html', context)
